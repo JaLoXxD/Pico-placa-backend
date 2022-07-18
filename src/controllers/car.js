d2ed62043c3b3586f8f0261e0f5779bcd9e1d59d
JorgeOrlando512 <jorgino91.jh@gmail.com>
2022-07-18 14:06:14 -0500

    [FIX] pico & placa rules
    add filter with the last number of the car identification

--- src/controllers/car.js
+++ src/controllers/car.js
@@ -28,12 +28,43 @@ class Car {
 	}
 	async checkCirculation(req, res) {
 		try {
-			const { placa } = req.body;
+			const { placa, date } = req.body;
+			if (new Date(date) < Date.now()) {
+				return res.status(200).json({
+					success: false,
+					message: `The date cannot be less than current date`,
+				});
+			}
+			const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+			const lastNumber = placa.slice(-1);
+			const day = new Date().getDay();
+			let allowedNumbers = [];
+			if (day === 1) {
+				allowedNumbers = [1, 2];
+			}
+			if (day === 2) {
+				allowedNumbers = [3, 4];
+			}
+			if (day === 3) {
+				allowedNumbers = [5, 6];
+			}
+			if (day === 4) {
+				allowedNumbers = [7, 8];
+			}
+			if (day === 5) {
+				allowedNumbers = [9, 0];
+			}
+			if (day === 6 || day === 7) {
+				allowedNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+			}
+			let allowed = false;
+			if (allowedNumbers.includes(parseInt(lastNumber))) {
+				allowed = true;
+			}
 			const filter = {
 				placa: placa,
 			};
 			const car = await this.carSchema.findOne(filter);
-            console.log(car)
 			if (!car) {
 				return res.status(200).json({
 					success: false,
@@ -45,9 +76,12 @@ class Car {
 				success: true,
 				message: "Car",
 				car,
+				lastNumber,
+				day: days[day - 1],
+				allowed,
 			});
 		} catch (err) {
-            console.log(err)
+			console.log(err);
 			return res.status(500).json({
 				success: false,
 				message: "There was an error...",
